381dc7a7d693ce9ad85ba06c09ed6d7aaafcb871
Murtaza Usmani <murtazausmani985@gmail.com>
2022-07-24 21:21:12 +0500

    Cleaned Up

--- Registration/admin.php
+++ Registration/admin.php
@@ -1,3 +1,18 @@
+<?php 
+
+include('./dbconnection.php');
+
+    $sql = $con-> prepare('select * from accountdetails');
+    $sql->execute();
+    $record = $sql->fetchALL(PDO::is_object);
+
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +20,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Welcome</h1>
-</body>
-</html>
+
+<div class="container">
+    <div class="row m-5">
+
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">ID</th>
+                <th scope="col">NAME</th>
+                <th scope="col">EMAIL</th>
+                <th scope="col">PASSWORD</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                foreach($record as $row){
+                    echo $row->id;
+                }
+                ?>
+                <tr>
+                <th scope="row"></th>
+                <td></td>
+                <td></td>
+                <td>@</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</div>
